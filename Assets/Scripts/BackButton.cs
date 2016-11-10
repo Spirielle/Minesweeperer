89cd74de138c2643ce5846479e1fd52395974c5b
Spirielle <spirielle@hotmail.com>
2016-11-09 20:48:28 -0500

    Added temporary back button

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour {
+    
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnMouseUpAsButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
