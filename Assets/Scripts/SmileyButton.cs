3dcbced0bc7aa681436329b13fb4a79997962af6
Spirielle <spirielle@hotmail.com>
2016-11-09 20:58:18 -0500

    The smile button now looks like it's being pressed when we press on it

--- Assets/Scripts/SmileyButton.cs
+++ Assets/Scripts/SmileyButton.cs
@@ -5,14 +5,15 @@ enum SmileyState {
     Default = 0,
     TileBeingPressed = 1,
     Victory = 2,
-    GameOver = 3
+    GameOver = 3,
+    SelfBeingPressed = 4
 }
 
 public class SmileyButton : MonoBehaviour {
 
     //TODO supi: find a way to add tooltip for each index of array
-    [Tooltip("0 = default, 1 = when tile pressed, 2 = victory, 3 = game over")]
-    public Sprite[] smileySprites = new Sprite[4];
+    [Tooltip("0 = default, 1 = when tile pressed, 2 = victory, 3 = game over, 4 = this button being pressed")]
+    public Sprite[] smileySprites = new Sprite[5];
     private GameManager gameManager;
 
     // Use this for initialization
@@ -39,5 +40,14 @@ public class SmileyButton : MonoBehaviour {
     void OnMouseUpAsButton()
     {
         gameManager.ResetGame();
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().sprite = smileySprites[(int)SmileyState.SelfBeingPressed];
+    }
+    void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().sprite = smileySprites[(int)SmileyState.Default];
     }
 }
